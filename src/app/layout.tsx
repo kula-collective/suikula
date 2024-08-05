39d4baef08191e4fb911be2fda3c6fb4012b7e6f
Shawn Lauzon <shawn.lauzon@gmail.com>
2024-08-05 18:29:30 -0500

    fix: Can't export metadata from a client component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,16 @@
 "use client";
 
 import { EnokiFlowProvider } from "@mysten/enoki/react";
-import type { Metadata } from "next";
+// import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "Let's Kula on Sui",
-  description: "The gift economy platform on Sui",
-};
+// export const metadata: Metadata = {
+//   title: "Let's Kula on Sui",
+//   description: "The gift economy platform on Sui",
+// };
 
 export default function RootLayout({
   children,
