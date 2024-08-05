4a26ed730349751743785882f28495f90deea9a9
Shawn Lauzon <shawn.lauzon@gmail.com>
2024-08-05 18:03:04 -0500

    Use useAuthCallback

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,3 +1,16 @@
+import { useAuthCallback } from "@mysten/enoki/react";
+import { useEffect } from "react";
+
 export default function Auth() {
-  return <h1>You have been authenticated!</h1>;
+  const { handled } = useAuthCallback();
+
+  useEffect(() => {
+    if (handled) {
+      // Get access token, perform security checks,
+      // manage user session, handle errors, and so on.
+      window.location.href = "/";
+    }
+  }, [handled]);
+
+  return <h1>Loading ...</h1>;
 }
