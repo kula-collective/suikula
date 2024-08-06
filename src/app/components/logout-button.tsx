a86266bd5fe02d6a3e62894ddb733462d8030672
Shawn Lauzon <shawn.lauzon@gmail.com>
2024-08-05 21:05:23 -0500

    feat(auth): Add logout button

---
+++ src/app/components/logout-button.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { useEnokiFlow } from "@mysten/enoki/react";
+
+export default function LogoutButton() {
+  const enokiFlow = useEnokiFlow();
+
+  const handleClick = () => {
+    enokiFlow.logout().then(() => {
+      window.location.href = "/";
+    });
+  };
+
+  return <button onClick={handleClick} />;
+}
--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,3 +1,12 @@
+import LogoutButton from "../components/logout-button";
+
 export default function Home() {
-  return <h1>Welcome to SuiKula</h1>;
+  return (
+    <>
+      <h1>Welcome to SuiKula</h1>
+      <div>
+        <LogoutButton />
+      </div>
+    </>
+  );
 }
