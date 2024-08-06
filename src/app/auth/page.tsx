81021c0472e082839bfe6b40b729258a9e0756b7
Shawn Lauzon <shawn.lauzon@gmail.com>
2024-08-05 20:36:47 -0500

    fix: Wrong number of segments in token: /auth

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -2,23 +2,23 @@
 
 import { verifyGoogle } from "@/app/lib/actions";
 import { useAuthCallback } from "@mysten/enoki/react";
-import { usePathname } from "next/navigation";
 import { useEffect } from "react";
 
 export default function Auth() {
   const { handled } = useAuthCallback();
-  const pathname = usePathname();
 
   useEffect(() => {
     if (handled) {
-      verifyGoogle(pathname).then((userId) => {
+      const hash = window.location.hash;
+      console.log("hash", hash);
+      verifyGoogle(hash).then((userId) => {
         console.log("Got userid" + userId);
         // Get access token, perform security checks,
         // manage user session, handle errors, and so on.
         window.location.href = "/home";
       });
     }
-  }, [handled, pathname]);
+  }, [handled]);
 
   return (
     <>
