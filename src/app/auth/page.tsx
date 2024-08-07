8bba4b6db7e8899e2fad7aa3de3f73c2c022028a
Shawn Lauzon <shawn.lauzon@gmail.com>
2024-08-06 21:28:21 -0500

    fix(auth): token disappears from url

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -2,21 +2,26 @@
 
 import { verifyGoogle } from "@/app/lib/actions";
 import { useAuthCallback } from "@mysten/enoki/react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Auth() {
   const { handled } = useAuthCallback();
+  const [token, setToken] = useState<string>();
 
   useEffect(() => {
-    if (handled) {
-      const hash = window.location.hash;
-      const token = hash.split("&")[0].substring("#id_token=".length);
+    // Store the token in state because it sometimes disappears
+    const hash = window.location.hash;
+    const idToken = hash.split("&")[0].substring("#id_token=".length);
+    if (idToken) {
+      setToken(idToken);
+    }
+    if (handled && token) {
       verifyGoogle(token)
         .then((user) => {
           console.log("user", user);
           // TODO store user
 
-          window.location.href = "/home?id=" + user.id;
+          //   window.location.href = "/home?id=" + user.id;
         })
         .catch((err) => {
           // TODO Display error
