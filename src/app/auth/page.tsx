2332749cca9cb0c5fbd35c02ed6597d85fcf6464
Shawn Lauzon <shawn.lauzon@gmail.com>
2024-09-10 16:15:31 -0500

    chore: diagnose not seeing avatar

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -8,7 +8,7 @@ import { useEffect, useState } from "react";
 export default function Auth() {
   const { handled } = useAuthCallback();
   const [token, setToken] = useState<string>();
-  const { login } = useAppstateStore((state) => state);
+  const { login, user } = useAppstateStore((state) => state);
 
   useEffect(() => {
     // Store the token in state because it sometimes disappears
@@ -22,8 +22,9 @@ export default function Auth() {
         .then((user) => {
           console.log("user", user);
           login(user);
+          console.log("User stored", user);
 
-          window.location.href = "/home?id=" + user.id;
+          // window.location.href = "/home?id=" + user.id;
         })
         .catch((err) => {
           // TODO Display error
