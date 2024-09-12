6ccfe7d99e4c3b633d84c74a8194ae06d2709495
Shawn Lauzon <shawn.lauzon@gmail.com>
2024-09-12 09:13:01 -0500

    fix: window not defined during prerender

--- src/app/oauth/google/page.tsx
+++ src/app/oauth/google/page.tsx
@@ -1,18 +1,22 @@
 "use client";
 
-export const dynamic = "force-dynamic";
-
 import { verifyGoogle } from "@/lib/actions";
 import { useAppstateStore } from "@/providers/appstate-store-provider";
 import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function Page() {
   const router = useRouter();
   const { saveUser } = useAppstateStore((state) => state);
+  const [idToken, setIdToken] = useState("");
 
-  // Must run on the client because the server doesn't get the hash
-  const hash = window.location.hash;
-  const idToken = hash.split("&")[0].substring("#id_token=".length);
+  useEffect(() => {
+    // Must run on the client because the server doesn't get the hash
+    const hash = window.location.hash;
+    const idToken = hash.split("&")[0].substring("#id_token=".length);
+    setIdToken(idToken);
+  }, []);
+
   if (!idToken) {
     return <>Missing token</>;
   }
