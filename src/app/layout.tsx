b33823650104ef34c2ed77b74d668693858da938
Shawn Lauzon <shawn.lauzon@gmail.com>
2024-09-12 17:35:37 -0500

    fix: build (0 , et.createContext) is not a function

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 // import type { Metadata } from "next";
 import { AppstateStoreProvider } from "@/providers/appstate-store-provider";
 import { AuthProvider } from "@/providers/auth-provider";
