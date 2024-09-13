21839fbd4b64c6f7f2b424a7db53b16c89498cce
Shawn Lauzon <shawn.lauzon@gmail.com>
2024-09-12 21:16:06 -0500

    feat: Show notification on Kula creation

--- src/components/create-kula-dialog.tsx
+++ src/components/create-kula-dialog.tsx
@@ -11,12 +11,15 @@ import {
 import { Field, Label } from "@/components/catalyst/fieldset";
 import { Input } from "@/components/catalyst/input";
 import { useEnoki } from "@/hooks/use-enoki";
+import { revalidatePath } from "next/cache";
 import { useRouter } from "next/navigation";
-import { FormEvent } from "react";
+import { FormEvent, useState } from "react";
+import Notification from "./notification";
 
 export default function CreateKulaDialog() {
   const router = useRouter();
   const enoki = useEnoki();
+  const [notificationVisible, setNotificationVisible] = useState(false);
 
   const onClose = () => router.back();
 
@@ -28,29 +31,40 @@ export default function CreateKulaDialog() {
     const formJson = Object.fromEntries(formData.entries());
     const kulaName = formJson["name"].toString();
 
-    await enoki.createKula(kulaName);
+    const kulaId = await enoki.createKula(kulaName);
+    revalidatePath("/kulas");
+    setNotificationVisible(true);
+    router.push(`/kulas/${kulaId}`);
   };
 
   return (
-    <Dialog open={true} onClose={onClose}>
-      <form onSubmit={handleSubmit}>
-        <DialogTitle>Create Kula</DialogTitle>
-        <DialogDescription>
-          This creates a new kula using the SUI in your account.
-        </DialogDescription>
-        <DialogBody>
-          <Field>
-            <Label>Name</Label>
-            <Input name="name" />
-          </Field>
-        </DialogBody>
-        <DialogActions>
-          <Button plain onClick={onClose}>
-            Cancel
-          </Button>
-          <Button type="submit">Create</Button>
-        </DialogActions>
-      </form>
-    </Dialog>
+    <>
+      <Dialog open={true} onClose={onClose}>
+        <form onSubmit={handleSubmit}>
+          <DialogTitle>Create Kula</DialogTitle>
+          <DialogDescription>
+            This creates a new kula using the SUI in your account.
+          </DialogDescription>
+          <DialogBody>
+            <Field>
+              <Label>Name</Label>
+              <Input name="name" />
+            </Field>
+          </DialogBody>
+          <DialogActions>
+            <Button plain onClick={onClose}>
+              Cancel
+            </Button>
+            <Button type="submit">Create</Button>
+          </DialogActions>
+        </form>
+      </Dialog>
+      {notificationVisible && (
+        <Notification
+          title="Kula created"
+          message="Now you can start adding offers"
+        />
+      )}
+    </>
   );
 }
